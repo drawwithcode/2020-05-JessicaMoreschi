b377b5c8801a2445235d5027bf67504077d6fa5b
JessicaMoreschi <71377205+JessicaMoreschi@users.noreply.github.com>
2020-11-24 15:48:02 +0100

    heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log("node is running")
 let express = require("express"); //Load the express code
 let socket = require("socket.io"); //Load the socket package
 let app = express(); //create local host
-let port = 3000; //dichiara server port (3000 standard)
+var port = process.env.PORT || 3000; //dichiara server port (3000 standard)
 let server = app.listen(port); //aspetta che qualcuno si connetta (da browser "localhost:3000")
 //per stoppare (ctrl+c); per riavviare "node server.js" (da terminale)
 
